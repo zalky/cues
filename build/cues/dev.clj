213ee84ce081a11d12f00236e6bbb2ba0842c7f1
zalky <zalan.k@gmail.com>
2023-02-16 20:25:23 -0500

    Added dev aliases

--- build/cues/dev.clj
+++ build/cues/dev.clj
@@ -6,6 +6,9 @@
          '[cues.deps :as deps]
          '[cues.repl :as repl]
          '[cues.queue :as q]
+         '[qbits.tape.appender :as app]
+         '[qbits.tape.queue :as queue]
+         '[qbits.tape.tailer :as tail]
          '[runway.core :as run]
          '[taoensso.timbre :as log])
 
